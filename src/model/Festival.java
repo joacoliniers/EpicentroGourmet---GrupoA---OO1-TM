ce45be0d479fa61ff78e6151e00abd4ec64e9c01
Agus <agusnieva0cinco@gmail.com>
2026-05-08 17:29:11 -0300

    feat: atributos de Sistema y Festival

--- src/model/Festival.java
+++ src/model/Festival.java
@@ -1,5 +1,76 @@
 package model;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Festival {
+	private int idFestival;
+	private String nombre;
+	private String temporada;
+	private LocalDate fechaInicio;
+	private LocalDate fechaFin;
+	private List<UnidadDeVenta> lstUnidadDeVentas;
+
+	public Festival(int idFestival, String nombre, String temporada, LocalDate fechaInicio, LocalDate fechaFin) {
+		super();
+		this.idFestival = idFestival;
+		this.nombre = nombre;
+		this.temporada = temporada;
+		this.fechaInicio = fechaInicio;
+		this.fechaFin = fechaFin;
+		this.lstUnidadDeVentas = new ArrayList<UnidadDeVenta>();
+	}
+
+	public int getIdFestival() {
+		return idFestival;
+	}
+
+	public void setIdFestival(int idFestival) {
+		this.idFestival = idFestival;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTemporada() {
+		return temporada;
+	}
+
+	public void setTemporada(String temporada) {
+		this.temporada = temporada;
+	}
+
+	public LocalDate getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(LocalDate fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public LocalDate getFechaFin() {
+		return fechaFin;
+	}
+
+	public void setFechaFin(LocalDate fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+
+	public List<UnidadDeVenta> getLstUnidadDeVentas() {
+		return lstUnidadDeVentas;
+	}
+
+	@Override
+	public String toString() {
+		return "Festival [idFestival=" + idFestival + ", nombre=" + nombre + ", temporada=" + temporada
+				+ ", fechaInicio=" + fechaInicio + ", fechaFin=" + fechaFin + ", lstUnidadDeVentas=" + lstUnidadDeVentas
+				+ "]";
+	}
 
 }
--- src/model/Sistema.java
+++ src/model/Sistema.java
@@ -1,5 +1,46 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sistema {
+	private List<Personal> lstPersonal;
+	private List<Festival> lstFestival;
+	private List<UnidadDeVenta> lstUnidades;
+	private double rendimientoEconomico;
+
+	public Sistema(double rendimientoEconomico) {
+		super();
+		this.lstPersonal = new ArrayList<Personal>();
+		this.lstFestival = new ArrayList<Festival>();
+		this.lstUnidades = new ArrayList<UnidadDeVenta>();
+		this.rendimientoEconomico = rendimientoEconomico;
+	}
+
+	public double getRendimientoEconomico() {
+		return rendimientoEconomico;
+	}
+
+	public void setRendimientoEconomico(double rendimientoEconomico) {
+		this.rendimientoEconomico = rendimientoEconomico;
+	}
+
+	public List<Personal> getLstPersonal() {
+		return lstPersonal;
+	}
+
+	public List<Festival> getLstFestival() {
+		return lstFestival;
+	}
+
+	public List<UnidadDeVenta> getLstUnidades() {
+		return lstUnidades;
+	}
+
+	@Override
+	public String toString() {
+		return "Sistema [lstPersonal=" + lstPersonal + ", lstFestival=" + lstFestival + ", lstUnidades=" + lstUnidades
+				+ ", rendimientoEconomico=" + rendimientoEconomico + "]";
+	}
 
 }
