7816585622d707059a8a5b76b5c0eaaabf2348aa
lucasmura <lucasmura@localhost>
2026-05-08 16:38:59 -0300

    feat: atributos de Cajero, Personal, Cocinero

--- src/model/Personal.java
+++ src/model/Personal.java
@@ -1,5 +1,78 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Personal {
+	private String nombre;
+	private String apellido;
+	private long dni;
+	private LocalDate fechaNacimineto;
+	private LocalDate fechaIngreso;
+	private Double sueldoBase;
+	
+	public Personal(String nombre, String apellido, long dni, LocalDate fechaNacimineto, LocalDate fechaIngreso,
+			Double sueldoBase) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+		this.fechaNacimineto = fechaNacimineto;
+		this.fechaIngreso = fechaIngreso;
+		this.sueldoBase = sueldoBase;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public long getDni() {
+		return dni;
+	}
+
+	public void setDni(long dni) {
+		this.dni = dni;
+	}
+
+	public LocalDate getFechaNacimineto() {
+		return fechaNacimineto;
+	}
+
+	public void setFechaNacimineto(LocalDate fechaNacimineto) {
+		this.fechaNacimineto = fechaNacimineto;
+	}
+
+	public LocalDate getFechaIngreso() {
+		return fechaIngreso;
+	}
+
+	public void setFechaIngreso(LocalDate fechaIngreso) {
+		this.fechaIngreso = fechaIngreso;
+	}
+
+	public Double getSueldoBase() {
+		return sueldoBase;
+	}
+
+	public void setSueldoBase(Double sueldoBase) {
+		this.sueldoBase = sueldoBase;
+	}
+
+	@Override
+	public String toString() {
+		return "Personal [nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni + ", fechaNacimineto="
+				+ fechaNacimineto + ", fechaIngreso=" + fechaIngreso + ", sueldoBase=" + sueldoBase + "]";
+	}
 
 }
