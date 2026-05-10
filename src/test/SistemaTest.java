5a347224873a7c03a041076b11f34649d7204292
Joaquin Liniers <liniersjoaquin@gmail.com>
2026-05-10 15:34:42 -0300

    fix

--- src/test/SistemaTest.java
+++ src/test/SistemaTest.java
@@ -13,7 +13,7 @@ public class SistemaTest {
 		
 		Sistema sistema = new Sistema();
 		Festival f = new Festival(1, "test", "test", LocalDate.of(2026, 1, 1), LocalDate.of(2026, 2, 1));
-		Personal p = new Personal("Lucas", "Murakoshi", 11111111, LocalDate.of(2004, 4, 20), LocalDate.of(2017, 10, 1), 100000000d);
+		Personal p = new Personal(1, "Lucas", "Murakoshi", 11111111, LocalDate.of(2004, 4, 20), LocalDate.of(2017, 10, 1), 100000000d);
 		UnidadDeVenta u = new UnidadDeVenta("Panchos Ricki", p , 100.00, 111111);
 		
 		try {
